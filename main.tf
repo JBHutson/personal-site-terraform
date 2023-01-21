0b3a6ca8e62044d55eafe3bcdaa9c1c5256e652e
james hutson <hutsonjb@gmail.com>
2023-01-20 21:33:27 -0500

    added s3 backend

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket   = "personal-site-terraform-state-james"
+    key      = "terraform.tfstate"
+    region   = "us-east-1"
+    role_arn = "arn:aws:iam::854241045080:user/terraform_admin"
+  }
+}
+
 module "vpc" {
-    source = "../modules/vpc"
+  source = "../modules/vpc"
 }
