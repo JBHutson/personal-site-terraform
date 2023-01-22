6fc65c5c7ffc13e65c236290ce9e4fccdb53f0c1
james hutson <hutsonjb@gmail.com>
2023-01-22 17:36:43 -0500

    added eks cluster and altered vpc to make it easier

--- main.tf
+++ main.tf
@@ -1,11 +1,28 @@
 terraform {
   backend "s3" {
-    bucket   = "personal-site-terraform-state-james"
-    key      = "terraform.tfstate"
-    region   = "us-east-1"
+    bucket = "personal-site-terraform-state-james"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
+locals {
+  eks_cluster_name = "test"
+}
+
 module "vpc" {
-  source = "./modules/vpc"
+  source       = "./modules/vpc"
+  cluster_name = local.eks_cluster_name
+}
+
+module "eks" {
+  source       = "./modules/eks"
+  cluster_name = local.eks_cluster_name
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+}
+
+module "iot" {
+  source = "./modules/iot"
 }
